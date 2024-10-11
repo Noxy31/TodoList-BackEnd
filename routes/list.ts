2a8614385fbb0f51574e308a8c7a1160f63f3f8b
Noxy31 <nellenoar@gmail.com>
2024-10-11 19:02:34 +0200

    Managed Lists Views with personnal lists et CreateList form

--- routes/list.ts
+++ routes/list.ts
@@ -2,22 +2,36 @@ import { Router, Request, Response } from 'express';
 import { query } from '../db';
 import authMiddleware from '../middlewares/authenticate';
 import enAccMiddleware from '../middlewares/isAccEnabled';
-import isAdminMiddleware from '../middlewares/isAdmin';
 
 const listRouter = Router();
 
 listRouter.post('/create', authMiddleware, enAccMiddleware, async (req: Request, res: Response) => {
   const { labelList, isPersonnal, idCategory } = req.body;
-
-
   const userId = (req as any).user.id;
 
   const listCreationTime = new Date();
+  const isArchived = false;
+  const listUpdateTime = null;
+  const archiveTime = null;
+  const idArchiver = null;
+
   try {
     const sql = `
-      INSERT INTO list (labelList, listCreationTime, isPersonnal, idUser, idCategory) 
-      VALUES (?, ?, ?, ?, ?)`;
-    const result = await query(sql, [labelList, listCreationTime, isPersonnal, userId, idCategory]);
+      INSERT INTO list (labelList, listCreationTime, listUpdateTime, archiveTime, isArchived, isPersonnal, idArchiver, idUser, idCategory) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    const result = await query(sql, [
+      labelList,
+      listCreationTime,
+      listUpdateTime,
+      archiveTime,
+      isArchived,
+      isPersonnal,
+      idArchiver,
+      userId,
+      isPersonnal ? null : idCategory,
+    ]);
 
     res.status(201).json({ message: 'Liste créée avec succès', listId: result.insertId });
   } catch (error) {
@@ -27,20 +41,20 @@ listRouter.post('/create', authMiddleware, enAccMiddleware, async (req: Request,
 });
 
 listRouter.get('/:userId', authMiddleware, enAccMiddleware, async (req: Request, res: Response) => {
-    const userId = req.params.userId;
-  
-    try {
-      const sql = `SELECT * FROM list WHERE idUser = ?`;
-      const lists = await query(sql, [userId]);
-  
-      if (lists.length === 0) {
-        return res.status(404).json({ message: 'Aucune liste trouvée pour cet utilisateur' });
-      }
-  
-      res.status(200).json(lists);
-    } catch (error) {
-      console.error('Erreur lors de la récupération des listes :', error);
-      res.status(500).json({ message: 'Erreur serveur lors de la récupération des listes' });
+  const userId = req.params.userId;
+
+  try {
+    const sql = `SELECT * FROM list WHERE idUser = ?`;
+    const lists = await query(sql, [userId]);
+
+    if (lists.length === 0) {
+      return res.status(404).json({ message: 'No list found for this user' });
     }
-  });
+
+    res.status(200).json(lists);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fecthing the lists' });
+  }
+});
+
 export default listRouter;
